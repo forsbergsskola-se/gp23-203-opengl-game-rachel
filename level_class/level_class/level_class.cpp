e976dd1d1532479ab14b9a2487f0a722e9e4d964
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-07 17:29:44 +0100

    Update: Wrote a method and if for gain experience

--- level_class/level_class/level_class.cpp
+++ level_class/level_class/level_class.cpp
@@ -17,7 +17,18 @@ class Level {
     Level(int start_level, int experience_per_level); 
 
 
-
+    // add method to gain experience 
+    void gain_experience(int more_experience) {
+        // adding more (gained) experience
+        experience += more_experience;
+        // if condition to check experience is enough 
+        if (experience >= experience_per_level) {
+            // decrease experience by level up 
+            experience -= experience_per_level;
+            // then level up 
+            level++; // add to level 
+        }
+    }
 
 
 
