16cfe95017dd768578ccb951f3b22d7f63b02a32
RachelLach <144023790+RachelLach@users.noreply.github.com>
2024-03-10 15:46:14 +0100

    Update: Added virtual attack method to public part in unit class

--- xxx_rpg/xxx_rpg/Skeleton.h
+++ xxx_rpg/xxx_rpg/Skeleton.h
@@ -18,12 +18,17 @@ public:
 	// writing constructor for initializing name and health
 	Unit(std::string name, int health) : name(name), health(health){}
 
-	// writing vitrual method to take damage
-	virtual void(take_damage) {
+	// writing virtual method to take damage
+	virtual void takeDamage(int damage){
 		health -= damage;
 	}
 
-	
+	// writing attack method on another unit 
+	// takes a reference to the target unit and the damage 
+	virtual void attack(Unit& target, int damage) {
+		std::cout << name << "attacks" << target.name << "" << std::endl;
+		target.takeDamage(damage);
+	}
 };
 
 int main() {
